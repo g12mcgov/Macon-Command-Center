b34933226216247e52c6323283e42de0e6cff5b0
Grant McGovern <grant@cobrain.com>
2015-08-30 16:08:46 -0400

    Updating with ngrok link

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ require('header/header.php');
 				  	<p>Grant's Room</p>
 				  	<div class="container-fluid">
 				  		<!-- Live feed of the security camera using mjpg o-->
-				  		<img id="security-feed" class="img-responsive" src="https://zdqqcmzs.p6.weaved.com/?action=stream"/>
+				  		<img id="security-feed" class="img-responsive" src="http://b77ad795.ngrok.io/?action=stream"/>
 				  	</div>
 				  </div>
 				</div>
